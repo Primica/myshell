d2016e9db85cf1d60658e092dcb791b0a46bad15
Primica <artbrun@icloud.com>
2024-12-16 16:57:20 +0100

    Enhance 'pwd' command to handle additional argument for file path resolution and improve error messaging

--- src/builtins/pwd.cpp
+++ src/builtins/pwd.cpp
@@ -4,11 +4,23 @@
 
 #include "myshell.h"
 
+#include <filesystem>
+
 void pwd(const std::vector<std::string>& args) {
-    if (args.size() != 1) {
+    if (args.size() > 2) {
         std::cerr << "pwd: too many arguments" << std::endl;
         return;
     }
-    std::cout << show_current_path() << std::endl;
+
+    if (args.size() == 2) {
+        std::filesystem::path filePath(args[1]);
+        if (std::filesystem::exists(filePath)) {
+            std::cout << std::filesystem::absolute(filePath).parent_path() << std::endl;
+        } else {
+            std::cerr << "pwd: " << args[1] << ": No such file or directory" << std::endl;
+        }
+    } else {
+        std::cout << show_current_path() << std::endl;
+    }
 }
 
